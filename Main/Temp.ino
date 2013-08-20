493bd20e0b7f4b821ccc121f0ca1277e670ba59c
Gael Grosch <gael.grosch@gmail.com>
2013-08-20 15:15:04 -0600

    Corrected some warnings

--- Main/Temp.ino
+++ Main/Temp.ino
@@ -1,55 +1,55 @@
 #ifdef TEMP_LIQ||TEMP_PLATE||TEMP_STEPPER
 
-#ifdef TEMP_CTRL
-
-  #ifdef TEMP_LIQ
-  OneWire oneWire1(TEMP_LIQ);
-  DallasTemperature sensors1(&oneWire1);
-  DeviceAddress oneWireAddress1;
-  #endif
-
-  #ifdef TEMP_PLATE
-  OneWire oneWire2(TEMP_PLATE);
-  DallasTemperature sensors2(&oneWire2);
-  DeviceAddress oneWireAddress2;
-  #endif
-
-#endif
-
-
-#ifdef STEPPER_CTRL
-
-  #ifdef TEMP_STEPPER
-  OneWire oneWire3(TEMP_STEPPER);
-  DallasTemperature sensors3(&oneWire3);
-  DeviceAddress oneWireAddress3;
+  #ifdef TEMP_CTRL
+  
+    #ifdef TEMP_LIQ
+      OneWire oneWire1(TEMP_LIQ);
+      DallasTemperature sensors1(&oneWire1);
+      DeviceAddress oneWireAddress1;
+    #endif
+  
+    #ifdef TEMP_PLATE
+      OneWire oneWire2(TEMP_PLATE);
+      DallasTemperature sensors2(&oneWire2);
+      DeviceAddress oneWireAddress2;
+    #endif
+  
   #endif
   
-#endif
-
-
-NIL_WORKING_AREA(waThreadTemp, 70);
-NIL_THREAD(ThreadTemp, arg) {
   
-  while(TRUE){
-  #ifdef TEMP_LIQ
-  getTemperature(sensors1, oneWireAddress1, TEMP_LIQ, PARAM_TEMP_LIQ);
-  #endif
-
-  #ifdef TEMP_PLATE
-  getTemperature(sensors2, oneWireAddress2, TEMP_PLATE, PARAM_TEMP_PLATE);
-  #endif
-
-  #ifdef TEMP_STEPPER
-  getTemperature(sensors3, oneWireAddress3, TEMP_STEPPER, PARAM_TEMP_STEPPER); 
+  #ifdef STEPPER_CTRL
+  
+    #ifdef TEMP_STEPPER
+      OneWire oneWire3(TEMP_STEPPER);
+      DallasTemperature sensors3(&oneWire3);
+      DeviceAddress oneWireAddress3;
+    #endif
+    
   #endif
   
-  nilThdSleepMilliseconds(500);
+  
+  NIL_WORKING_AREA(waThreadTemp, 70);
+  NIL_THREAD(ThreadTemp, arg) {
+    
+    while(TRUE){
+    #ifdef TEMP_LIQ
+      getTemperature(sensors1, oneWireAddress1, TEMP_LIQ, PARAM_TEMP_LIQ);
+    #endif
+  
+    #ifdef TEMP_PLATE
+      getTemperature(sensors2, oneWireAddress2, TEMP_PLATE, PARAM_TEMP_PLATE);
+    #endif
+  
+    #ifdef TEMP_STEPPER
+      getTemperature(sensors3, oneWireAddress3, TEMP_STEPPER, PARAM_TEMP_STEPPER); 
+    #endif
+    
+    nilThdSleepMilliseconds(500);
+    }
   }
-}
-
-
-
+  
+  
+  
 void getTemperature(DallasTemperature sensor, DeviceAddress address, int bus, int parameter){
    sensor.begin();
   if (!sensor.getAddress(address, 0)) debugger(DEBUG_ONEWIRE_NODEVICE, bus); 
