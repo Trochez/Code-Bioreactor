ea0e4446f036c9196314d4969a0bfe5fa4ca1843
qcabrol <quentin.cabrol@gmail.com>
2013-08-28 15:56:11 -0500

    weight implementation bis

--- Main/Weight.ino
+++ Main/Weight.ino
@@ -8,24 +8,31 @@ NIL_THREAD(ThreadWeight, arg) {
       //sensor read
       weight = analogRead(WGHT);
       setParameter(PARAM_WGHT, weight);
-      
-      //add a sanity check if the value is out of range of if the weight does not change after pumping for a while (ERROR_WGHT_CTRL)
-      //condition would restart the mixing and disable pumping
+     
 
-      if(weight>=PARAM_LVL_MAX_WATER){
+      if(weight>=getParameter(PARAM_LVL_MAX_WATER)){
         //set the condition to initiate pumping sequence
         setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR)||FLAG_STEPPER_OFF));
-        time=now();        
+        int time=now();        
         while((now()-time)<=getParameter(PARAM_WAIT_TIME_PUMP_MOTOR)){
            weight = analogRead(WGHT);
            setParameter(PARAM_WGHT, weight);
            nilThdSleepMilliseconds(1000);  
         }
+        //enable pumping
         setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR)||FLAG_PUMPING));
         
+      //add a sanity check here if the value is out of range of if the weight does not change after pumping for a while (ERROR_WGHT_CTRL)
+      //condition would restart the mixing and disable pumping
+        /*
+        while(weight>=getParameter(PARAM_LVL_MIN_WATER)){
+           weight = analogRead(WGHT);
+           setParameter(PARAM_WGHT, weight);
+           nilThdSleepMilliseconds(1000);  
+        }  */
       }
       
-      if(weight<=PARAM_LVL_MIN_WATER){
+      if(weight<=getParameter(PARAM_LVL_MIN_WATER)){
         //set the condition to disable the pumping sequence and re-anable stepper motor mixing
         setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR)&~(FLAG_STEPPER_OFF)));
         setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR)&~(FLAG_PUMPING)));
