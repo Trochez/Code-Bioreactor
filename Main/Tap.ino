246e17ed940a176315440dcf0e170dafdf532b97
qcabrol <quentin.cabrol@gmail.com>
2013-09-02 17:56:47 -0500

    tap control

--- Main/Tap.ino
+++ Main/Tap.ino
@@ -6,19 +6,40 @@
 NIL_WORKING_AREA(waThreadTap, 50);
 NIL_THREAD(ThreadTap, arg) { 
   
-  unsigned long previous_time_food=now();
-  unsigned long previous_time_ph=now();
-  unsigned long previous_time_gas=now();
+  #define OPEN  1
+  #define CLOSE 0
+  
+  #ifdef TAP_FOOD
+    unsigned long previous_time_food=now();
+    boolean food_state=CLOSE;
+    setParameter(FOOD_PERIOD,10000);        //one pulse every 10 sec, default config
+  #endif
+  
+  
+//  unsigned long previous_time_ph=now();
+//  unsigned long previous_time_gas=now();
   
   while(true){
   
+    
+/***********
+ FOOD SUPPLY
+************/
+
     #ifdef TAP_FOOD
-      if( (now()-previous_time_food) <= OPENING_TIME )
+    
+      if( ((now()-previous_time_food) >= getParameter(FOOD_PERIOD)) & (food_state==CLOSE))
       {
-        setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<8)); 
+        setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<8)); //open the tap
+        previous_time_food=now();
+        food_state=OPEN;
       }
       
-      
+      else if( ((now()-previous_time_food) >= OPENING_TIME) & (food_state==OPEN))
+      {
+        setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<0)); //close the tap
+        food_state=CLOSE;
+      }
       
     #endif
     
