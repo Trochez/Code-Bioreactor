2c589ce3072ccfb351e4cdd2e9d49ec336826185
qcabrol <quentin.cabrol@gmail.com>
2013-09-02 16:51:43 -0500

    tap control starting implementation

--- Main/Tap.ino
+++ Main/Tap.ino
@@ -6,33 +6,20 @@
 NIL_WORKING_AREA(waThreadTap, 50);
 NIL_THREAD(ThreadTap, arg) { 
   
-  #ifdef TAP_FOOD
-    
-  #endif
-  
-  #ifdef
-  #endif
-  
-  #ifdef
-  #endif
+  unsigned long previous_time_food=now();
+  unsigned long previous_time_ph=now();
+  unsigned long previous_time_gas=now();
   
   while(true){
- 
-    #ifdef  TAP_GAS1
-    #endif
-    
-    #ifdef  TAP_GAS2
-    #endif
-    
-    #ifdef  TAP_GAS3
-    #endif
-    
-    #ifdef  TAP_GAS4
-    #endif
   
     #ifdef TAP_FOOD
+      if( (now()-previous_time_food) <= OPENING_TIME )
+      {
+        setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<8)); 
+      }
       
-      setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<8)); 
+      
+      
     #endif
     
     /*PH implementation 
@@ -50,6 +37,18 @@ NIL_THREAD(ThreadTap, arg) {
       setParameter(PARAM_RELAY_TAP,getParameter(PARAM_RELAY_TAP)|(8<<8));
     #endif 
     */
+    
+    #ifdef  TAP_GAS1
+    #endif
+    
+    #ifdef  TAP_GAS2
+    #endif
+    
+    #ifdef  TAP_GAS3
+    #endif
+    
+    #ifdef  TAP_GAS4
+    #endif
   }
   
 }
