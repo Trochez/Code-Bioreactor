45d41c38f6e31960c93832f36a9d7b1f912af281
qcabrol <quentin.cabrol@gmail.com>
2013-09-04 16:40:58 -0500

    event logs and EEPROM set check added

--- Main/Weight.ino
+++ Main/Weight.ino
@@ -4,11 +4,30 @@ NIL_WORKING_AREA(waThreadWeight, 32);
 NIL_THREAD(ThreadWeight, arg) {
     
     //quick init of the default parameters
-    setParameter(PARAM_WAIT_TIME_PUMP_MOTOR,10);    //wait time of 10 seconds, to be changed then
-    setParameter(PARAM_LVL_MAX_WATER,420);
-    setParameter(PARAM_LVL_MIN_WATER,370);
-    setParameter(PARAM_RELAY_PUMP,0);
-    setParameter(FLAG_VECTOR,0);
+    if(getParameter(PARAM_WAIT_TIME_PUMP_MOTOR) != MAX_INTEGER)
+    {
+      setAndSaveParameter(PARAM_WAIT_TIME_PUMP_MOTOR,10);    //wait time of 10 seconds, to be changed then
+    }  
+
+    if(getParameter(PARAM_LVL_MAX_WATER) != MAX_INTEGER)
+    {    
+      setAndSaveParameter(PARAM_LVL_MAX_WATER,420);
+    }
+      
+    if(getParameter(PARAM_LVL_MIN_WATER) != MAX_INTEGER)
+    {    
+      setAndSaveParameter(PARAM_LVL_MIN_WATER,370); 
+    }  
+      
+    if(getParameter(PARAM_RELAY_PUMP) != MAX_INTEGER)  
+    {
+      setAndSaveParameter(PARAM_RELAY_PUMP,0);
+    }
+      
+    if(getParameter(FLAG_VECTOR) != MAX_INTEGER)
+    {
+      setAndSaveParameter(FLAG_VECTOR,0);
+    }
     
     int leaky_wght;
     int weight = analogRead(WGHT);
@@ -27,6 +46,7 @@ NIL_THREAD(ThreadWeight, arg) {
         if(weight>=getParameter(PARAM_LVL_MAX_WATER)){
           //set the condition to initiate pumping sequence
           setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR) | FLAG_STEPPER_OFF));
+          writeLog(COMMAND_LOGS, &newEntryCmd, now(), MOTOR_STOP, 0);
           unsigned long time=now();        
           while((now()-time)<=getParameter(PARAM_WAIT_TIME_PUMP_MOTOR)){
              leaky_wght=weight;
@@ -37,6 +57,7 @@ NIL_THREAD(ThreadWeight, arg) {
           //enable pumping
           setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR) | FLAG_PUMPING));
           setParameter(PARAM_RELAY_PUMP,(getParameter(PARAM_RELAY_PUMP)|8));
+          writeLog(COMMAND_LOGS, &newEntryCmd, now(), PUMPING_START, 0);
           
         //add a sanity check here if the value is out of range of if the weight does not change after pumping for a while (ERROR_WGHT_CTRL)
         //condition would restart the mixing and disable pumping
@@ -52,8 +73,10 @@ NIL_THREAD(ThreadWeight, arg) {
           //set the condition to disable the pumping sequence and re-anable stepper motor mixing
           setParameter(PARAM_RELAY_PUMP, (getParameter(PARAM_RELAY_PUMP)&~8));
           setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR) & ~(FLAG_PUMPING)));
+          writeLog(COMMAND_LOGS, &newEntryCmd, now(), PUMPING_STOP, 0);
           nilThdSleepMilliseconds(1000); 
           setParameter(FLAG_VECTOR,(getParameter(FLAG_VECTOR) & ~(FLAG_STEPPER_OFF)));
+          writeLog(COMMAND_LOGS, &newEntryCmd, now(), MOTOR_START, 0);
 
         }
       
