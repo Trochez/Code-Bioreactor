b28a20c4b5d60efa38a8aee6034e9df220ad50c6
Luc Patiny <luc@patiny.com>
2014-01-25 00:27:28 +0100

    Update ethernet

--- MainWORRD/Logs_Ethernet_thread.ino
+++ MainWORRD/Logs_Ethernet_thread.ino
@@ -46,20 +46,13 @@
 #define ASCII_a 97
 #define ASCII_z 122
 
-//Define the bytes of the URL in the request from the client
-#define GET_CHAR_1 5
-#define GET_CHAR_2 6
-#define GET_CHAR_3 7
 
 //The longest request possible is "GET /s=4294967295"
-#define REQUEST_LENGTH 17
+#define REQUEST_LENGTH 20
+#define REQUEST_START  4
+
+
 #define TABLE_SIZE 32
-
-
-
-#ifdef THR_LINEAR_LOGS
-void printIndexes(Print* output);
-#endif
 
 
 uint8_t ip[] = IP;
@@ -73,7 +66,7 @@ EthernetServer server(80);
  * Ethernet Thread
  *****************/
 
-NIL_WORKING_AREA(waThreadEthernet, 500); //change memoy allocation
+NIL_WORKING_AREA(waThreadEthernet, 600); //change memoy allocation
 NIL_THREAD(ThreadEthernet, arg) {
 
   Ethernet.begin(mac,ip);
@@ -97,7 +90,7 @@ NIL_THREAD(ThreadEthernet, arg) {
 
     if(getParameter(FLAG_VECTOR) & EVENT_OCCURED){
 #ifdef THR_LINEAR_LOGS
-  //    writeLog(getParameter(PARAM_EVENT), getParameter(PARAM_EVENT_VALUE)); 
+      //   writeLog(getParameter(PARAM_EVENT), getParameter(PARAM_EVENT_VALUE)); 
 #endif
       setParameter(FLAG_VECTOR, getParameter(FLAG_VECTOR) & (~EVENT_OCCURED));
     }
@@ -115,47 +108,40 @@ NIL_THREAD(ThreadEthernet, arg) {
       Serial.println("new client");
 #endif
       // an http request ends with a blank line
-      boolean currentLineIsBlank = true;
+      boolean starting = true;
       //Count the number of byte of the answer
 
-      uint8_t request[TABLE_SIZE];
+      char request[TABLE_SIZE];
       int count = 0;
-      while (client.connected()) {
 
+      while (client.connected()) {
         if (client.available()) {
           char c = client.read();
+          
           //store characters to string           
-          if (count < REQUEST_LENGTH) {
-            // += append a character to a string
-            request[count] = c;
-            count++;
-          }
-          // if you've gotten to the end of the line (received a newline
-          // character) and the line is blank, the http request has ended,
-          // so you can send a reply
-          if (c == '\n' && currentLineIsBlank) {
-            // send a standard http response header
-            client.println(F("HTTP/1.1 200 OK"));
-            client.println(F("Content-Type: text/html"));
-            client.println(F("Connection: close"));  // the connection will be closed after completion of the response
-            client.println();
-            client.println(F("<!DOCTYPE HTML>"));
-            client.println(F("<html>"));
-            parseRequest(&client, request);
+          if (starting && count<REQUEST_LENGTH && count>=REQUEST_START) {
+            Serial.print(c);
 
-            client.println(F("</html>"));
-            break;
+            // += append a character to a string
+            request[count-REQUEST_START] = c;
+
+            if (c=='\n' || c=='\r' || c==' ') starting=false;
           }
-          if (c == '\n') {
-            // you're starting a new line
-            currentLineIsBlank = true;
-          } 
-          else if (c != '\r') {
-            // you've gotten a character on the current line
-            currentLineIsBlank = false;
-          }
+          count++;
         }
       }
+
+
+      client.println(F("HTTP/1.1 200 OK"));
+      client.println(F("Content-Type: text/html"));
+      client.println(F("Connection: close"));  // the connection will be closed after completion of the response
+      client.println();
+      client.println(F("<!DOCTYPE HTML>"));
+      client.println(F("<html><pre>"));
+      printResult(request, &client);
+      client.println(F("</pre></html>"));
+
+
       // give the web browser time to receive the data
       delay(1);
       // close the connection:
@@ -163,9 +149,7 @@ NIL_THREAD(ThreadEthernet, arg) {
 #ifdef DEBUG_ETHERNET
       Serial.println("client disconnected");
 #endif
-      /*while(client.status() != 0) {
-       delay(5);
-       }*/
+
     } 
 #endif
 
@@ -180,29 +164,8 @@ NIL_THREAD(ThreadEthernet, arg) {
 
 void parseRequest(Client* cl, uint8_t* req) {
 
-    noSuchCommand(cl); 
-
+  noSuchCommand(cl); 
 }
-
-uint32_t getNumber(uint8_t start, uint8_t* tab){
-  uint32_t index = 0;
-  uint8_t i = start;
-  uint8_t c = tab[i];
-
-  while(c >= ASCII_0  && c <= ASCII_9 && i<REQUEST_LENGTH){
-    //Conversion in integers : The letters start at index 48 in ASCII
-    index = (c-ASCII_0) + index*10;
-    i++;
-    c = tab[i];
-  }
-  return index; 
-}
-
-/****************************************
- * Parameter & requests related functions
- *****************************************/
-
-
 
 
 
@@ -217,6 +180,10 @@ void noThread(Print* output){
 
 
 #endif
+
+
+
+
 
 
 
