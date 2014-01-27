cd4a4806f2d0caa5ac81c7e663d014ea39904cb7
lpatiny <test@tsf-460-wpa-7-109.epfl.ch>
2014-01-27 13:45:55 +0100

    Update node script

--- Node/request.js
+++ Node/request.js
@@ -1,42 +1,63 @@
 var http = require('http');
-
-var options = {
-  host: '172.17.0.105',
-  port: 80,
-  path: '/l'
-};
+var nano = require('nano')('http://localhost:5984');
 
 
-tmp = []  
+var arduinoIPs=["172.17.0.105"]
 
-var req = http.request(options, function(res) {
-  res.setEncoding('utf8');
+setInterval(function() {
+  for (var i=0; i<arduinoIPs.length; i++) {
+    var ip=arduinoIPs[i];
+
+    // we need to search for the last entryID for this specific IP address
+    getLastEntryID(ip, function(ip, lastID) {
+      fetchLog(ip, lastID+1);
+    })
+  }
+
+},5000)
+
+
+
+
+
+
+function fetchLog(host, lastID) {
+  var tmp = [];
+  var options={
+    host: host,
+    path: "/m"+lastID
+  };
+
+console.log(options);
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
     res.on('data', function(chunk) {
-      tmp.push(chunk)
+      tmp.push(chunk);
     });
     res.on('end', function (e) {
-      var body = tmp.join('');
-      parseResult(body);
+       var body = tmp.join('');
+       parseResult(body, options);
+    });
   });
-});
 
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
 
-// write data to request body
-req.write('data\n');
-req.end();
-
+  // write data to request body
+  req.end();
+}
 
 
-function parseResult(result) {
+function parseResult(result, options) {
   var lines=result.split(/[\r\n]+/);
   for (var i=0; i<lines.length; i++) {
     var line=lines[i];
     var entry={};
-    if (line.length!=134) {
-        console.log("Not correct length: "+line);
+    if (line.length>0) {
+      if (line.length!=134) {
+          console.log("Not correct length: "+line);
       } else {
         if (checkDigit(line)) {
           entry.id=parseInt("0x"+line.substring(0,8));
@@ -47,18 +68,19 @@ function parseResult(result) {
           entry.event=parseInt("0x"+line.substring(120,124));
           entry.eventParameter=parseInt("0x"+line.substring(124,128));
           entry.mac=line.substring(128,132);
+          entry.host=options.host;
+          saveToCouchDB(entry);
         } else {
           console.log("Check digit error: "+line);
         }
+      }
     }
-    console.log(entry);
   }
 
   function checkDigit(line) {
     var checkDigit=0;
     for (var i=0; i<line.length; i=i+2) {
       checkDigit^=parseInt("0x"+line[i]+line[i+1]);
-      console.log(checkDigit)
     }
     if (checkDigit==0) return true;
     return false;
@@ -72,3 +94,30 @@ function parseResult(result) {
     return value;
   }
 }
+
+function getCouchDBLink() {
+  nano.db.create('bioreactor');
+  return nano.db.use('bioreactor');   
+}
+
+function getLastEntryID(host, callback) {
+  var bioreactor =getCouchDBLink();    
+  bioreactor.view('logDisplay', 'stats', {key:"172.17.0.105"}, function(err, body) {
+    if (!err) {
+      callback(host, body.rows[0].value.max);
+    } else {
+      console.log("ERROR: can not getLastEntryID from: "+host);
+    }
+  });
+}
+
+
+function saveToCouchDB(entry) {
+  var bioreactor =getCouchDBLink();     
+  bioreactor.insert( entry, function(err, body, header) {
+    if (err) throw console.log(err);
+    // console.log(header);
+  });
+}
+
+
