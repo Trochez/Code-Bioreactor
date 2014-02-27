5f1d1f0e02d14a6bd9634cbcf09c450c802e2568
lpatiny <test@tsf-460-wpa-3-220.epfl.ch>
2014-02-27 14:05:47 +0100

    Improve error managment

--- Node/request.js
+++ Node/request.js
@@ -114,8 +114,8 @@ function get(host, options, callback) {
     }).on('error', function(e) {
       console.log('problem with request: ' + e.message);
     })
-   request.setTimeout(3000, function() {
-     request.abort();
+    request.setTimeout(3000, function() {
+      request.abort();
       console.log("Request timeout");
     });
 
@@ -170,24 +170,47 @@ function parseResult(result, options) {
 }
 
 function getCouchDBLink() {
-  nano.db.create(dbName);
-  return nano.db.use(dbName);   
+  try {
+    //nano.db.create(dbName);
+    //console.log("XXXXX")
+    var couchdb=nano.db.use(dbName);
+    return couchdb;
+  } catch (err) {
+    console.log(err.description)
+    return false;
+  }
+ 
 }
 
 function getLastEntryID(host, callback) {
   var bioreactor =getCouchDBLink();
-  bioreactor.view('logDisplay', 'stats', {key:host}, function(err, body) {
-    if (!err) {
-      var lastID=-1;
-      if (body.rows && body.rows[0] && body.rows[0].value && body.rows[0].value.max) {
-        lastID=body.rows[0].value.max;
-      }
-      callback(host, lastID);
-    } else {
-      console.log("ERROR: can not getLastEntryID: Assuming 0");
-      callback(host, -1);
+  try {
+      bioreactor.view('logDisplay', 'stats', {key:host}, function(err, body) {
+        if (!err) {
+          var lastID=-1;
+          if (body.rows && body.rows[0] && body.rows[0].value && body.rows[0].value.max) {
+            lastID=body.rows[0].value.max;
+          }
+          callback(host, lastID);
+        } else {
+              console.log(err.description);
+          if (err && err.description) {
+            if (err.description=="no_db_file") {
+              console.log("ERROR: the database does not exists");
+            } else  if (err.description=="missing") {
+              console.log("ERROR: the view document stats is missing");
+             } else  if (err.description=="XXXXXX") {
+              console.log("ERROR: can not getLastEntryID: Assuming 0");
+              callback(host, -1);
+            } else {
+              console.log("ERROR: "+err.description);
+            }
+          }
+        }
+      });
+    } catch (err) {
+      console.log("ERROR: "+err)
     }
-  });
 }
 
 
